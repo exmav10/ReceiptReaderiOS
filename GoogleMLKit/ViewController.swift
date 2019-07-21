b0093543f8dbd0915867e9e819c035408179d380
Aydin Unal [Uygulama Gelistirme - Mobil Bankacilik Uygulamalari Bolumu] <Aydin.Unal@ingbank.com.tr>
2019-07-21 22:22:44 +0300

    Initial Commit

---
+++ GoogleMLKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleMLKit
+//
+//  Created by Aydin Unal [Uygulama Gelistirme - Mobil Bankacilik Uygulamalari Bolumu] on 21.07.2019.
+//  Copyright © 2019 Aydin Unal [Uygulama Gelistirme - Mobil Bankacilik Uygulamalari Bolumu]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
